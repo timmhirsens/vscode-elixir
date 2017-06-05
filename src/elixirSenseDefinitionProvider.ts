70f2ac4e35755adc087623b01226d09cb6590add
Potter Dai <potterdaica@gmail.com>
2017-06-05 09:39:43 -0400

    Fix and cleanning follow-up.

--- src/elixirSenseDefinitionProvider.ts
+++ src/elixirSenseDefinitionProvider.ts
@@ -48,31 +48,6 @@ export class ElixirSenseDefinitionProvider implements vscode.DefinitionProvider 
 
                 resolve(location);
             });
-
-            /*
-            this.server.getDefinition(document, position, (res: string) => {
-                if (res === 'non_existing' || res === '' || res === undefined || res === null) {
-                    resolve(null);
-                } else {
-                    const splitAt = res.lastIndexOf(':');
-                    let location;
-                    if (splitAt !== 1) {
-                        const filePath = res.substring(0, splitAt);
-                        const lineNumber = parseInt(res.substring(splitAt + 1, res.length)) - 1;
-                        if (lineNumber >= 0) {
-                            //TODO: Need to find the correct (character) position here:
-                            location = new vscode.Location(vscode.Uri.file(filePath), new vscode.Position(lineNumber, 0));
-                        } else {
-                            location = new vscode.Location(vscode.Uri.file(filePath), new vscode.Position(0, 0));
-                        }
-                    } else {
-                        location = new vscode.Location(vscode.Uri.file(res), new vscode.Position(0, 0));
-                    }
-                    resolve(location);
-                }
-            });
-            */
-
         });
     }
 }
