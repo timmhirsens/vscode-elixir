85e8c925af322e319edb920ecaea27b9233176bd
Samuel Volin <untra.sam@gmail.com>
2017-07-15 19:10:36 -0600

    renamed configuration.ts, lint elixirMain.ts

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
 
 export const configuration: vscode.LanguageConfiguration = {
-  wordPattern: /:?\w+[\.\w+]*/g,
+  wordPattern: /:?\w+[\.\w+]*/g, //? is this correct?
   indentationRules: {
-    increaseIndentPattern: new RegExp('(after|else|catch|rescue|fn|^.*(do|<\\-|\\->|\\{|\\[))\\s*$'),
-    decreaseIndentPattern: new RegExp('^\\s*((\\}|\\])\\s*$|(after|else|catch|rescue|end)\\b)')
-  }
-};
+    increaseIndentPattern: new RegExp("(after|else|catch|rescue|fn|^.*(do|<\\-|\\->|\\{|\\[))\\s*$"),
+    decreaseIndentPattern: new RegExp("^\\s*((\\}|\\])\\s*$|(after|else|catch|rescue|end)\\b)")
+  },
+
+}
