320a2c92b1e3ef18014a74765953c44feae63304
Samuel Volin <untra.sam@gmail.com>
2017-07-15 19:19:30 -0600

    linted the rest

--- src/elixirAutocomplete.ts
+++ src/elixirAutocomplete.ts
@@ -9,6 +9,7 @@ export class ElixirAutocomplete implements vscode.CompletionItemProvider {
         this.elixirServer = elixirServer;
     }
 
+    // tslint:disable-next-line:max-line-length
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.CompletionItem[] | Thenable<vscode.CompletionItem[]> {
         return new Promise<vscode.CompletionItem[]>((resolve, reject) => {
             this.elixirServer.getCompletions(document, position, (result: vscode.CompletionItem[]) => {
