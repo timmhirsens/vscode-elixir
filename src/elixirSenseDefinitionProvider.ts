c68f1d8722026ed06e4336ea73deee3873a5096a
Samuel Volin <untra.sam@gmail.com>
2017-07-15 19:06:58 -0600

    lint elixirSenseDefinitionProvider.ts

--- src/elixirSenseDefinitionProvider.ts
+++ src/elixirSenseDefinitionProvider.ts
@@ -1,5 +1,4 @@
 import * as vscode from 'vscode';
-//import { ElixirSense } from './elixirSense';
 import { ElixirSenseClient } from './elixirSenseClient';
 
 export class ElixirSenseDefinitionProvider implements vscode.DefinitionProvider {
@@ -9,20 +8,25 @@ export class ElixirSenseDefinitionProvider implements vscode.DefinitionProvider 
         this.elixirSenseClient = elixirSenseClient;
     }
 
-    provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.Definition | Thenable<vscode.Definition> {
+    provideDefinition(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.Definition> {
 
         const wordAtPosition = document.getWordRangeAtPosition(position);
         const word = document.getText(wordAtPosition);
         return new Promise<vscode.Definition>((resolve, reject) => {
 
             if (!this.elixirSenseClient) {
-                console.log("ElixirSense client not ready");
+                console.log('ElixirSense client not ready');
                 console.error('rejecting');
                 reject();
                 return;
             }
+            const payload = {
+                buffer : document.getText(),
+                line   : position.line + 1,
+                column : position.character + 1
+            };
 
-            this.elixirSenseClient.send("definition", { buffer: document.getText(), line: position.line + 1, column: position.character + 1 }, result => {
+            this.elixirSenseClient.send('definition', payload, (result) => {
 
                 if (token.isCancellationRequested) {
                     console.error('rejecting');
@@ -30,12 +34,12 @@ export class ElixirSenseDefinitionProvider implements vscode.DefinitionProvider 
                     return;
                 }
 
-                let filePath = result.substring(0, result.lastIndexOf(":"));
-                let lineNumberStr = result.substring(result.lastIndexOf(":") + 1, result.length);
-                let lineNumber = Number(lineNumberStr) - 1;
+                const [filePath, lineNumberStr] = result.split(':');
 
-                if (!filePath || filePath == 'non_existing') {
-                    resolve(null);
+                const lineNumber = Number(lineNumberStr) - 1;
+
+                if (!filePath || filePath === 'non_existing') {
+                    resolve(undefined);
                     return;
                 }
 
