c1f75aa7b8f80ac89c7631b59b8c16f3cad70614
Samuel Volin <untra.sam@gmail.com>
2017-07-27 00:02:58 -0600

    Rewrite and lint - cleanup (#75)
    * installed tslint and rules
    
    * lint jem.ts
    
    * lint elixirServer.ts
    
    * lint elixirSenseClient.ts and elixirSenseSignatureHelpProvider.ts
    
    * lint elixirSenseServerProcess.ts
    
    * lint elixirSenseHoverProvider.ts
    
    * lint elixirSenseDefinitionProvider.ts
    
    * lint and refactor elixirSenseAutocompleteProvider.ts
    
    * renamed configuration.ts, lint elixirMain.ts
    
    * lint elixirMain.ts
    
    * linted the rest
    
    * missed a spot
    Release v0.5.0
    * added elixirsense subscription concatenations

--- src/elixirMain.ts
+++ src/elixirMain.ts
@@ -1,41 +1,46 @@
 import * as vscode from 'vscode';
+import { configuration } from './configuration';
 import { ElixirAutocomplete } from './elixirAutocomplete';
-import { ElixirServer } from './elixirServer';
 import { ElixirDefinitionProvider } from './elixirDefinitionProvider';
 import { ElixirHoverProvider } from './elixirHoverProvider';
-import { configuration } from './elixirConfiguration';
-//Elixir-Sense
-import { ElixirSenseServerProcess } from './elixirSenseServerProcess';
-import { ElixirSenseClient } from './elixirSenseClient';
 import { ElixirSenseAutocompleteProvider } from './elixirSenseAutocompleteProvider';
+import { ElixirSenseClient } from './elixirSenseClient';
 import { ElixirSenseDefinitionProvider } from './elixirSenseDefinitionProvider';
 import { ElixirSenseHoverProvider } from './elixirSenseHoverProvider';
+import { ElixirSenseServerProcess } from './elixirSenseServerProcess';
 import { ElixirSenseSignatureHelpProvider } from './elixirSenseSignatureHelpProvider';
+import { ElixirServer } from './elixirServer';
 
 const ELIXIR_MODE: vscode.DocumentFilter = { language: 'elixir', scheme: 'file' };
+// tslint:disable-next-line:prefer-const
 let elixirServer: ElixirServer;
-//Elixir-Sense
+// Elixir-Sense
 let useElixirSense: boolean;
 let elixirSenseServer: ElixirSenseServerProcess;
 let elixirSenseClient: ElixirSenseClient;
 
-
 export function activate(ctx: vscode.ExtensionContext) {
-    let elixirSetting = vscode.workspace.getConfiguration('elixir');
+    const elixirSetting = vscode.workspace.getConfiguration('elixir');
     useElixirSense = elixirSetting.useElixirSense;
 
     if (useElixirSense) {
         ElixirSenseServerProcess.initClass();
-        //TODO: detect environment automatically.
-        let env = elixirSetting.elixirEnv;
-        let projectPath = vscode.workspace.rootPath;
-        elixirSenseServer = new ElixirSenseServerProcess(vscode.workspace.rootPath, function (host, port, auth_token) {
-            elixirSenseClient = new ElixirSenseClient(host, port, auth_token, env, projectPath);
-            ctx.subscriptions.push(vscode.languages.registerCompletionItemProvider(ELIXIR_MODE, new ElixirSenseAutocompleteProvider(elixirSenseClient), '.', '{', '@'));
-            ctx.subscriptions.push(vscode.languages.registerDefinitionProvider(ELIXIR_MODE, new ElixirSenseDefinitionProvider(elixirSenseClient)));
-            ctx.subscriptions.push(vscode.languages.registerHoverProvider(ELIXIR_MODE, new ElixirSenseHoverProvider(elixirSenseClient)));
-            ctx.subscriptions.push(vscode.languages.registerSignatureHelpProvider(ELIXIR_MODE, new ElixirSenseSignatureHelpProvider(elixirSenseClient), '(', ','));
-            ctx.subscriptions.push(vscode.languages.setLanguageConfiguration('elixir', configuration));
+        // TODO: detect environment automatically.
+        const env = elixirSetting.elixirEnv;
+        const projectPath = vscode.workspace.rootPath;
+        elixirSenseServer = new ElixirSenseServerProcess(vscode.workspace.rootPath, (host, port, authToken) => {
+            elixirSenseClient = new ElixirSenseClient(host, port, authToken, env, projectPath);
+            const autoCompleteProvider = new ElixirSenseAutocompleteProvider(elixirSenseClient);
+            const definitionProvider = new ElixirSenseDefinitionProvider(elixirSenseClient);
+            const hoverProvider = new ElixirSenseHoverProvider(elixirSenseClient);
+            const signatureHelpProvider = new ElixirSenseSignatureHelpProvider(elixirSenseClient);
+            ctx.subscriptions.concat([
+                vscode.languages.registerCompletionItemProvider(ELIXIR_MODE, autoCompleteProvider, '.', '{', '@'),
+                vscode.languages.registerDefinitionProvider(ELIXIR_MODE, definitionProvider),
+                vscode.languages.registerHoverProvider(ELIXIR_MODE, hoverProvider),
+                vscode.languages.registerSignatureHelpProvider(ELIXIR_MODE, signatureHelpProvider, '(', ','),
+                vscode.languages.setLanguageConfiguration('elixir', configuration)
+            ]);
         });
         elixirSenseServer.start(0, env);
     } else {
